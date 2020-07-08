2284651b42700920095c7088f34463e32d6c1f1d
masyuke <67623673+masyuke@users.noreply.github.com>
2020-07-08 15:48:10 +0900

    update-message-send

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    patams.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email)
   end
 
 end
