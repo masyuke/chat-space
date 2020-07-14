134d3927afaaaf6fb2152c4436d72baf67ce7a99
masyuke <67623673+masyuke@users.noreply.github.com>
2020-07-14 11:37:23 +0900

    asynchronous-communication

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module ChatSpace
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     end
   end
 end
